0986760606b1971105e72b484a203c58403fb7b2
AlexBeltran31 <alexbeltranrt@gmail.com>
2026-02-20 12:22:59 +0100

    Setup base architecture with folder structure and providers

---
+++ src/app/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white">
+      <h1 className="text-3xl font-bold text-center pt-10">
+        QueerReads Frontend
+      </h1>
+    </div>
+  )
+}
+
+export default App
